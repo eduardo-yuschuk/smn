75685cab93c0f0d4df860c9a70d3513242e6ee5b
eduardo.yuschuk <eduardo.yuschuk@gmail.com>
2021-12-06 13:40:57 -0300

    -

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -51,21 +51,174 @@ pub struct WeatherReport {
 }
 
 impl WeatherReport {
-    pub fn create() -> WeatherReport {
-        let response = reqwest::blocking::get("https://ws.smn.gob.ar/map_items/forecast/1")
+    pub fn create(days: i32) -> WeatherReport {
+        let url = format!("https://ws.smn.gob.ar/map_items/forecast/{}", days);
+        let response = reqwest::blocking::get(&url)
             .expect(&format!("Error al acceder al servicio en la URL {}!", url));
         WeatherReport {
             reports: response
                 .json::<Vec<Report>>()
-                .expect("Error al interpretar la respuesta del servicio!"),
+                .expect("Error al interpretar la respueta del servicio!"),
         }
+    }
+
+    pub fn get_provinces(&self) -> Vec<String> {
+        let mut provinces: Vec<String> = self
+            .reports
+            .as_slice()
+            .into_iter()
+            .map(|report| report.province.clone())
+            .collect();
+        provinces.sort();
+        provinces.dedup();
+        provinces
+    }
+
+    pub fn get_cities(&self, province: &str) -> Vec<String> {
+        let mut cities: Vec<String> = self
+            .reports
+            .as_slice()
+            .into_iter()
+            .filter(|report| report.province.eq_ignore_ascii_case(province))
+            .map(|report| report.name.clone())
+            .collect();
+        cities.sort();
+        cities
+    }
+
+    pub fn get_all_cities(&self) -> Vec<String> {
+        let mut cities: Vec<String> = self
+            .reports
+            .as_slice()
+            .into_iter()
+            .map(|report| report.name.clone())
+            .collect();
+        cities.sort();
+        cities
+    }
+
+    pub fn get_report(&self, province: &str, city: &str) -> &Report {
+        self.reports
+            .as_slice()
+            .into_iter()
+            .filter(|report| {
+                report.province.eq_ignore_ascii_case(province)
+                    && report.name.eq_ignore_ascii_case(city)
+            })
+            .collect::<Vec<&Report>>()
+            .first()
+            .expect("")
+    }
+}
+
+/// lectura a través de un puntero a función
+pub fn get_weather_reports<F>(days: i32, function: F)
+where
+    F: Fn(&Report),
+{
+    let url = format!("https://ws.smn.gob.ar/map_items/forecast/{}", days);
+    let response = reqwest::blocking::get(&url)
+        .expect(&format!("Error al acceder al servicio en la URL {}!", url));
+    let reports = response
+        .json::<Vec<Report>>()
+        .expect("Error al interpretar la respueta del servicio!");
+    for report in reports {
+        function(&report);
     }
 }
 
 fn main() {
-    let weather_report = WeatherReport::create();
+    println!("Buscamos el pronóstico a 1, 2 o 3 días?:");
+    let weather_report = WeatherReport::create(read_int_from_stdin());
+
+    println!("Seleccione una provincia (ingrese el número):");
+    let provinces = weather_report.get_provinces();
+    for (id, province) in provinces.iter().enumerate() {
+        println!("[{}] {}", id, province);
+    }
+    let province = provinces
+        .get(read_int_from_stdin() as usize)
+        .expect("Provincia no encontrada!");
+
+    println!("Seleccione una ciudad:");
+    let cities = weather_report.get_cities(province);
+    for (id, city) in cities.iter().enumerate() {
+        println!("[{}] {}", id, city);
+    }
+    let city = cities
+        .get(read_int_from_stdin() as usize)
+        .expect("Ciudad no encontrada!");
 
     let city_report = weather_report.get_report(province, city);
     println!("{}", city_report.weather);
 }
 
+pub fn procedural_app() {
+    // captura de pedido del usuario
+    println!("Buscamos el pronóstico a 1, 2 o 3 días?:");
+    let days = read_int_from_stdin();
+    // lectura de datos
+    println!("Buscando el pronóstico a {} días...", days);
+    let url = format!("https://ws.smn.gob.ar/map_items/forecast/{}", days);
+    let response = reqwest::blocking::get(&url)
+        .expect(&format!("Error al acceder al servicio en la URL {}!", url));
+    let reports = response
+        .json::<Vec<Report>>()
+        .expect("Error al interpretar la respueta del servicio!");
+    // presentación de los datos, provincias
+    let mut provinces: Vec<String> = reports
+        .as_slice()
+        .into_iter()
+        .map(|report| report.province.clone())
+        .collect();
+    provinces.sort();
+    provinces.dedup();
+    println!("Seleccione una provincia (ingrese el número):");
+    for (id, province) in provinces.iter().enumerate() {
+        println!("[{}] {}", id, province);
+    }
+    let province_id = read_int_from_stdin();
+    let province = provinces
+        .get(province_id as usize)
+        .expect("Provincia no encontrada!");
+    println!("Provincia seleccionada: {}...", province);
+    // ciudades
+    println!("Seleccione una ciudad:");
+    let mut cities = vec![];
+    for report in reports
+        //.as_slice()
+        .iter()
+    {
+        if report.province.eq_ignore_ascii_case(province) {
+            cities.push(report.name.clone());
+        }
+    }
+    cities.sort();
+    for (id, city) in cities.iter().enumerate() {
+        println!("[{}] {}", id, city);
+    }
+    // pronóstico para la ciudad seleccionada
+    let city_id = read_int_from_stdin();
+    let city = cities.get(city_id as usize).expect("Ciudad no encontrada!");
+    println!("Ciudad seleccionada: {}...", city);
+    //let city_report: Vec<Report> =
+    for city_report in reports
+        //.as_slice()
+        .into_iter()
+        .filter(|report| report.name.eq_ignore_ascii_case(city))
+    {
+        println!("{}", city_report.weather);
+    }
+}
+
+fn read_int_from_stdin() -> i32 {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error al leer el valor");
+    let value = buffer
+        .trim()
+        .parse::<i32>()
+        .expect("Error al interpretar el valor");
+    value
+}
